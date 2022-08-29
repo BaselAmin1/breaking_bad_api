5eaf42d82de2dba7d6957edf560311c90db38fba
basselamin <98540858+basselamin@users.noreply.github.com>
2022-08-29 18:07:55 +0200

    first commit

---
+++ ios/business_logic/cubit/characters_state.dart
@@ -0,0 +1,17 @@
+part of 'characters_cubit.dart';
+
+@immutable
+abstract class CharactersState {}
+
+class CharactersInitial extends CharactersState{}
+class CharactersLoaded extends CharactersState{
+  final List<Character> character;
+
+  CharactersLoaded(this.character);
+}
+class QuotesLoaded extends CharactersState {
+  final List<Quote> quotes;
+
+  QuotesLoaded(this.quotes);
+}
+
